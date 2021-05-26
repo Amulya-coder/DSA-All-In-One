06ba9ea85e485a327c34e9f87ceece2230596fdc
Amulya Dixit <amulyadixit07@gmail.com>
2021-05-27 00:40:47 +0530

    Add Palindrome Linked list

--- Leetcode/Palindrome_Linked_List.cpp
+++ Leetcode/Palindrome_Linked_List.cpp
@@ -1,5 +1,42 @@
 bool isPalindrome(ListNode* head){
 
+ListNode* slow=head;
+ListNode* fastptr=head;
 
+while(fastptr && fastptr->next){
+    slow=slow->next;
+    fastptr=fastptr->next->next;
+
+}
+
+slow->next=reverseList(slow->next);
+slow=slow->next;
+
+while(slow!=NULL){
+    if(head->val!=slow->val){
+        return false;
+    slow=slow->next;
+    head=head->next
+    }
     
+
+}
+ return true;
+
+}
+
+void reverseList(ListNode* head){
+ ListNode* prevptr=NULL;
+ ListNode* currptr=head;
+ ListNode* nextptr;
+
+while(currptr!=NULL){
+    nextptr=currptr->next;
+    currptr->next=prevptr;
+
+    prevptr=currptr;
+    currptr=nextptr;
+}
+return prevptr;
+
 }
