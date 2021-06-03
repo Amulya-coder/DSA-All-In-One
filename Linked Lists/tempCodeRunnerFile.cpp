c68d8fa19bc5d650198c447f28c91da18b6335ad
Amulya Dixit <amulyadixit07@gmail.com>
2021-06-04 00:11:00 +0530

    Update Circular Linked List

---
+++ Linked Lists/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+void deleteAtHead(node* &head){
+//     node* temp=head;
+//     while(temp->next!=head){
+//         temp=temp->next;
+//     }
+ 
+//     node* todelete=head;
+//     temp->next=head->next;
+//     head=head->next;
+ 
+//     delete todelete;
+// }
