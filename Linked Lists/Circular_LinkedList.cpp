d095460dc11fe664c67e3edadfb65da5159cfad0
Amulya Dixit <amulyadixit07@gmail.com>
2021-06-04 23:16:37 +0530

    Update Circular Linked list

--- Linked Lists/Circular_LinkedList.cpp
+++ Linked Lists/Circular_LinkedList.cpp
@@ -49,38 +49,48 @@ void insertAtTail(node* &head, int val){
     n->next=head;
 }
  
-// void deleteAtHead(node* &head){
-//     node* temp=head;
-//     while(temp->next!=head){
-//         temp=temp->next;
-//     }
- 
-//     node* todelete=head;
-//     temp->next=head->next;
-//     head=head->next;
- 
-//     delete todelete;
-// }
- 
-// void deletion(node* &head, int pos){
- 
-//     if(pos==1){
-//         deleteAtHead(head);
-//         return;
-//     }
-//       node* temp=head;
-//       int count=1;
- 
-//       while(count!=pos-1){
-//           temp=temp->next;
-//           count++;
-//       }
- 
-//       node* todelete=temp->next;
-//       temp->next=temp->next->next;
- 
-//       delete todelete;
-// }
+void deleteAtHead(node* &head){
+
+node* temp=head;
+
+while(temp->next!=head){
+
+temp=temp->next;
+
+}
+
+node* todelete=head;
+temp->next=head->next;
+head=head->next;
+
+delete todelete;
+
+}
+
+void deletion(node* &head,int pos){
+
+if(pos==1){
+
+deleteAtHead(head);
+return;
+}
+
+node* temp=head;
+int count=1;
+
+while(count!=pos-1){
+    temp=temp->next;
+    count++;
+}
+
+node* todelete=temp->next;
+temp->next=temp->next->next;
+
+delete todelete;
+
+}
+
+
  
 void display(node* head){
  
@@ -104,10 +114,10 @@ int main(){
     insertAtTail(head,3);
     insertAtTail(head,4);
     display(head);
-    insertAtHead(head,5);
+    insertAtHead(head,8);
     display(head);
-    // deletion(head,1);
-    // display(head);
+    deletion(head,2);
+    display(head);
     
  
     return 0;
