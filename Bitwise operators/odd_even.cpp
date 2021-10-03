d55c10a7f2b2416694170c546736d5274a1c3544
Amulya Dixit <amulyadixit07@gmail.com>
2021-10-03 23:42:13 +0530

    odd_even

---
+++ Bitwise operators/odd_even.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+
+	int n=100;
+//& operator to find out the last bit
+	if(n&1){
+		cout<<"odd"<<endl;
+	}
+	else{
+		cout<<"even"<<endl;
+	}
+
+
+	return 0;
+}
