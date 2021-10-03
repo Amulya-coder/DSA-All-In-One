16c937a4624d4c5874fdc36b7c642b3fa3aa45c0
Amulya Dixit <amulyadixit07@gmail.com>
2021-10-03 23:47:00 +0530

    Power of two

---
+++ Bitwise operators/Power_of_two.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+if((n&(n-1))==0){
+	cout<<"Power of two"<<endl;
+}
+else{
+	cout<<"Not power of two"<<endl;
+}
+
+}
