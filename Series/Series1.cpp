4f234e18d5f21edcccbe61e862f2c8cbf4db6731
Amulya Dixit <amulyadixit07@gmail.com>
2021-10-28 22:36:12 +0530

    Series Problem 1

---
+++ Series/Series1.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//1 2 3 6 9 18 27
+
+//
+int main(){
+
+	int n,first=1,second=2,c;
+	cin>>n;
+
+	for(int i=1;i<=n;i++){
+
+
+		if(i==1){
+			cout<<first<<" ";
+		}
+		else if(i==2){
+			cout<<second<<" ";
+		}
+		else{
+			if(i%2==1){
+				c=first+second;
+				cout<<c<<" ";
+				first=second;
+				second=c;
+			}
+			else{
+				c=2*second;
+				cout<<c<<" ";
+				first=second;
+				second=c;
+			}
+		}
+	}
+
+
+
+	return 0;
+}
