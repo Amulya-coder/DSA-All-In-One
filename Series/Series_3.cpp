28c551a9ce60ad441474c2afdaa23e084c2b2a80
Amulya Dixit <amulyadixit07@gmail.com>
2021-10-28 22:44:54 +0530

    Problem on Series 3

---
+++ Series/Series_3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+// n=5
+// 2 15 41 80 132
+
+int main() {
+	
+    // your code goes here
+    int n;
+    cin>>n;
+
+    int val=2;
+
+    for(int i=1;i<=n;i++){
+        cout<<val<<" ";
+        val=val+13*i;
+    }
+
+
+	return 0;
+}
+
