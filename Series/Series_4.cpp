bffb7545b3e817c59e1c63522cfd21e1b9dbaf1f
Amulya Dixit <amulyadixit07@gmail.com>
2021-10-29 22:18:17 +0530

    Update

--- Series/Series_4.cpp
+++ Series/Series_4.cpp
@@ -1,23 +1,38 @@
-#include <iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-// n=5
-// 2 15 41 80 132
+/*
 
-int main() {
-	
-    // your code goes here
+n=5
+0.5 1.5 4.5 13.5 40.5
+0.5
+1.5 - 0.5+3^0 
+4.5 - 1.5+3^1
+13.5 - 4.5+3^2
+40.5 - 13.5+3^3 
+*/
+
+int main(){
     int n;
     cin>>n;
-
-    int val=2;
-
-    for(int i=1;i<=n;i++){
-        cout<<val<<" ";
-        val=val+13*i;
+    double r=0.5;
+    
+    
+    for(int i=0;i<n;i++){
+        
+        if(i==0){
+            cout<<r<<" ";
+            continue;
+        }
+        else{
+        double t=pow(3,i-1);
+        double x=t+r;
+        r=x;
+        cout<<x<<" ";
+        }
     }
-
-
-	return 0;
+    
+    
+    return 0;
 }
-
