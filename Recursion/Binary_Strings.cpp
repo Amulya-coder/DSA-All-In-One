165797fe04e075e423d516c220c608318ee29c7b
Amulya Dixit <amulyadixit07@gmail.com>
2022-01-15 23:21:15 +0530

    Correction

--- Recursion/Binary_Strings.cpp
+++ Recursion/Binary_Strings.cpp
@@ -1,51 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 vector<string> v;
 
-void helper(string str,int n,int i){
-	if(i==n){
+void helper(string str, int n, int i)
+{
+	if (i == n)
+	{
 		v.push_back(str);
 		return;
 	}
 
-	int s1=str;
+	int s1 = str;
 	s1.push_back('0');
-	helper(s1,n,i+1);
+	helper(s1, n, i + 1);
 
-	if(i>0 and str[i-1]!='1'){
+	if (i > 0 and str[i - 1] != '1')
+	{
 		str.push_back('1');
-		helper(str,n,i+1);
-	}						
-	
-	else{
+		helper(str, n, i + 1);
+	}
+
+	else
+	{
 		str.push_back('1');
-		helper(str,n,i+1);
+		helper(str, n, i + 1);
 	}
 
 	return;
-
 }
 
-vector<string> BinaryString(int n){
+vector<string> BinaryString(int n)
+{
 
 	v.clear();
 	string str;
-	helper(str,n,0);
-	
+	helper(str, n, 0);
+
 	return v;
-
 }
 
+int main()
+{
+	vector<string> res = BinaryString(3);
 
-int main(){
-vector<string> res=BinaryString(3);
+	for (int i = 0; i < 3; i++)
+	{
 
-for(int i=0;i<3;i++){
+		cout << res[i] << " ";
+	}
 
-	cout<<res[i]<<" ";
-}
-
-return 0;
+	return 0;
 }
