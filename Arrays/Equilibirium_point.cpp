4226dd46894bc43db9914f04778c9a4c530dc6fe
Amulya Dixit <amulyadixit07@gmail.com>
2022-05-06 23:01:52 +0530

    Find Equilbirium point

--- Arrays/Equilibirium_point.cpp
+++ Arrays/Equilibirium_point.cpp
@@ -1,38 +1,80 @@
-#include<bits/stdc++.h>
+/* GFG Practice
+Given an array A of n positive numbers. The task is to find the first Equilibium Point in the array.
+Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+
+Note: Retun the index of Equilibrium point. (1-based index)
+
+Example 1:
+
+Input:
+n = 5
+A[] = {1,3,5,2,2}
+Output: 3
+Explanation: For second test case
+equilibrium point is at position 3
+as elements before it (1+3) =
+elements after it (2+2).
+
+
+Example 2:
+
+Input:
+n = 1
+A[] = {1}
+Output: 1
+Explanation:
+Since its the only element hence
+its the only equilibrium point.
+
+
+Your Task:
+The task is to complete the function equilibriumPoint() which takes the array and n as input parameters and returns the point of equilibrium. Return -1 if no such point exists.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= n <= 10^6
+1 <= A[i] <= 10^8
+*/
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-        int n;
-        cin>>n;
+    int n;
+    cin >> n;
 
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int sum = 0;
+    int sum_so_far = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+    }
+
+    if (n == 1)
+    {
+        cout << 1 << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum - a[i];
+        if (sum == sum_so_far)
+        {
+            cout << i + 1 << endl;
+            break;
         }
-
-        int sum=0;
-        int sum_so_far=0;
-        for(int i=0;i<n;i++){
-            sum+=a[i];
-        }
-
-        if(n==1){
-            return 1;
-        }
-
-        else{
-            for(int i=0;i<n;i++){
-                sum=sum-a[i];
-                if(sum==sum_so_far){
-                cout<<i+1<<endl;
-                break;
-                }
-                sum_so_far=sum_so_far+a[i];
-                // cout<<i<<" "<<sum_so_far<<endl;
-            }
-        }
-
-    // return -1;
+        sum_so_far = sum_so_far + a[i];
+        // cout<<i<<" "<<sum_so_far<<endl;
+    }
 }
