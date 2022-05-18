c80cb49fdd1d6a63e1395a1f5559c8a1fc5833b2
Amulya Dixit <amulyadixit07@gmail.com>
2022-05-18 23:51:09 +0530

    Find the K Beauty of a number

---
+++ Strings/find_the_K_Beauty_of_a_number.cpp
@@ -0,0 +1,78 @@
+/*
+Leetcode Bi-weekly contest problem
+The k-beauty of an integer num is defined as the number of substrings of num when it is read as a string that meet the following conditions:
+
+It has a length of k.
+It is a divisor of num.
+Given integers num and k, return the k-beauty of num.
+
+Note:
+
+Leading zeros are allowed.
+0 is not a divisor of any value.
+A substring is a contiguous sequence of characters in a string.
+
+Example 1:
+
+Input: num = 240, k = 2
+Output: 2
+Explanation: The following are the substrings of num of length k:
+- "24" from "240": 24 is a divisor of 240.
+- "40" from "240": 40 is a divisor of 240.
+Therefore, the k-beauty is 2.
+Example 2:
+
+Input: num = 430043, k = 2
+Output: 2
+Explanation: The following are the substrings of num of length k:
+- "43" from "430043": 43 is a divisor of 430043.
+- "30" from "430043": 30 is not a divisor of 430043.
+- "00" from "430043": 0 is not a divisor of 430043.
+- "04" from "430043": 4 is not a divisor of 430043.
+- "43" from "430043": 43 is a divisor of 430043.
+Therefore, the k-beauty is 2.
+
+Constraints:
+
+1 <= num <= 10^9
+1 <= k <= num.length (taking num as a string)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int divisorSubstrings(int num, int k)
+    {
+        string s = to_string(num);
+        string str = "";
+        int count = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (i < k)
+            {
+                str.push_back(s[i]);
+            }
+            else
+            {
+                int x = stoi(str);
+                if (x != 0 and num % x == 0)
+                {
+                    count++;
+                }
+                str.push_back(str[i]);
+                str.erase(0, 1);
+            }
+        }
+
+        int x = stoi(str);
+        if (x != 0 and num % x == 0)
+        {
+            count++;
+        }
+        return count;
+    }
+};
