d21f75b51ef35ce0dbee5f76db102987a25dc111
Amulya Dixit <amulyadixit07@gmail.com>
2022-05-19 22:46:43 +0530

    Alternate positive and negative numbers

---
+++ Strings/Alternate_positive_negative_numbers.cpp
@@ -0,0 +1,77 @@
+/*
+GFG
+
+Given an unsorted array Arr of N positive and negative numbers. Your task is to create an array of alternate positive and negative numbers without changing the relative order of positive and negative numbers.
+Note: Array should start with positive number.
+
+
+Example 1:
+
+Input:
+N = 9
+Arr[] = {9, 4, -2, -1, 5, 0, -5, -3, 2}
+Output:
+9 -2 4 -1 5 -5 0 -3 2
+Example 2:
+
+Input:
+N = 10
+Arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
+Output:
+5 -5 2 -2 4 -8 7 1 8 0
+
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function rearrange() which takes the array of integers arr[] and n as parameters. You need to modify the array itself.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 ≤ N ≤ 10^7
+-106 ≤ Arr[i] ≤ 10^7
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    // Arr[] = {9, 4, -2, -1, 5, 0, -5, -3, 2}
+    void rearrange(int arr[], int n)
+    {
+
+        vector<int> v1, v2;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] >= 0)
+            {
+                v1.push_back(arr[i]);
+            }
+            else
+            {
+                v2.push_back(arr[i]);
+            }
+        }
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < v1.size() and j < v2.size())
+        {
+
+            arr[k++] = v1[i++];
+            arr[k++] = v2[j++];
+        }
+        while (i < v1.size())
+        {
+            arr[k++] = v1[i++];
+        }
+        while (j < v2.size())
+        {
+            arr[k++] = v2[j++];
+        }
+    }
+};
