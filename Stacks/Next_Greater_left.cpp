05c42bee6387dc1fe8b42f15d0bf898486b4d1a8
Amulya Dixit <amulyadixit07@gmail.com>
2022-06-07 23:56:55 +0530

    Next Greater left

---
+++ Stacks/Next_Greater_left.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    // Using stack
+    // Print the immediate next greater element if exists else -1
+
+    // Time complexity: for every element at worst case is both pushed and
+    // pop O(2n) = O(n)
+    //  vector<int> ngr={4, 5, 2, 25};
+    vector<int> ngr = {25, 4, 5, 2};
+
+    stack<int> st;
+
+    int n = ngr.size();
+
+    vector<int> ans(n, -1);
+
+    for (int i = 0; i < n; i++)
+    {
+
+        int curr = ngr[i];
+
+        while (!st.empty() and st.top() < curr)
+        {
+            st.pop();
+        }
+
+        if (!st.empty())
+        {
+            ans[i] = st.top();
+        }
+
+        st.push(curr);
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+}
