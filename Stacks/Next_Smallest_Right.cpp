72562a27fd1e450ef73b8ffd83f0a5a4e6f7a5d3
Amulya Dixit <amulyadixit07@gmail.com>
2022-06-07 23:58:35 +0530

    Next Smallest Right

---
+++ Stacks/Next_Smallest_Right.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// find the immediate next smaller right
+// Print the indexes if element exists else -1
+int main()
+{
+
+    // vector<int> nsr={4, 5, 3, 25};
+    vector<int> nsr = {89, 32, 41, 51, 2, 11, 1};
+
+    int n = nsr.size();
+    vector<int> ans(n, -1);
+
+    stack<int> st;
+    for (int i = n - 1; i >= 0; i--)
+    {
+
+        while (!st.empty() and nsr[st.top()] >= nsr[i])
+        {
+            st.pop();
+        }
+
+        if (!st.empty())
+        {
+            ans[i] = st.top();
+        }
+
+        st.push(i);
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
