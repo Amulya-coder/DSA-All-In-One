5364ab982bd7c2993cfd183c08b2c9389eeafbba
Amulya Dixit <amulyadixit07@gmail.com>
2022-06-25 22:17:09 +0530

    More Optimised Solution

--- Queue/first_non_repeating_character_in_stream.cpp
+++ Queue/first_non_repeating_character_in_stream.cpp
@@ -4,7 +4,42 @@ using namespace std;
 
 int main()
 {
+    string s;
+    cin >> s;
 
+    // More optimised
+    // T.C. O(n) and S.C O(26) ~ O(1) (taking map of 26 letters)
+    // not using queue
+    unordered_map<int, int> mp;
+    string ans = "";
+    int pointer = 0;
+    int index = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        mp[s[i] - 'a']++;
+
+        while (pointer <= index and mp[s[i] - 'a'] > 1)
+        {
+            pointer++;
+        }
+        if (pointer > index)
+        {
+            ans += '#';
+        }
+        else
+        {
+            ans += s[pointer];
+        }
+        index++;
+    }
+
+    for (int i = 0; i < ans.length(); i++)
+    {
+        cout << ans[i];
+    }
+
+    // Time Complexity O(n) and Space Complexity O(n)
+    /*
     string s;
     cin >> s;
 
@@ -31,4 +66,6 @@ int main()
         {
             cout << "#";
         }
-    }
+        */
+    return 0;
+}
