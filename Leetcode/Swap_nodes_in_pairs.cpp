9a3d912e0d05fe4b1cc11309211f00bf6a41af98
Amulya Dixit <amulyadixit07@gmail.com>
2023-03-31 23:30:30 +0530

    Update Swap nodes in pairs

--- Leetcode/Swap_nodes_in_pairs.cpp
+++ Leetcode/Swap_nodes_in_pairs.cpp
@@ -1,31 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Solution {
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
 public:
-    ListNode* swapPairs(ListNode* head) {
-        if(!head)return head;
-        else if(!head->next)return head;
-        else{
-            ListNode* temp=new ListNode(); temp=head;
-            head=head->next;
-            while(temp->next){
-                ListNode* i=temp->next;ListNode*j=temp->next->next;
-                if(!j){
-                    temp->next=j;
-                    i->next=temp;
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (!head)
+            return head;
+        else if (!head->next)
+            return head;
+        else
+        {
+            ListNode *temp = new ListNode();
+            temp = head;
+            head = head->next;
+            while (temp->next)
+            {
+                ListNode *i = temp->next;
+                ListNode *j = temp->next->next;
+                if (!j)
+                {
+                    temp->next = j;
+                    i->next = temp;
                     break;
                 }
-                else if(!j->next){
-                    temp->next=j;
-                    i->next=temp;
+                else if (!j->next)
+                {
+                    temp->next = j;
+                    i->next = temp;
                     break;
                 }
-                else{
-                    temp->next=j->next;
-                    i->next=temp;
-                    temp=j;
+                else
+                {
+                    temp->next = j->next;
+                    i->next = temp;
+                    temp = j;
                 }
             }
             return head;
