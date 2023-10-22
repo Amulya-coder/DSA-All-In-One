b8c238780bce501a632783a3768ab167da93904a
Amulya Dixit <amulyadixit07@gmail.com>
2023-10-23 00:07:11 +0530

    Lower Bound

--- Sorting & Searching/Implement Lower Bound.cpp
+++ Sorting & Searching/Implement Lower Bound.cpp
@@ -6,7 +6,15 @@ int lowerBound(vector<int> arr, int n, int x)
 {
     // Write your code here
     // Lower bound - smallest for which arr[ind]>=x
+    // Lower bound - smallest for which arr[ind]>=x
 
+    //[2,3,6,8,8,9,10] x=6
+    // Ans - 6 ind - 2
+    //[2,3,6,8,8,9,10] x=8
+    // Ans- 8 ind- 3
+
+    //[2,3,6,8,9,10] x=11
+    // Ans=n -> 7
     int low = 0;
     int high = n - 1;
 
