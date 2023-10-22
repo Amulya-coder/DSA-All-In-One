196d659796c5f159c2b7a4d32d4a7dae687708a5
Amulya Dixit <amulyadixit07@gmail.com>
2023-10-23 00:06:36 +0530

    Upper Bound

--- Sorting & Searching/Implement Upper Bound.cpp
+++ Sorting & Searching/Implement Upper Bound.cpp
@@ -1,30 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int upperBound(vector<int> &arr, int x, int n){
-	// Write your code here.	
+int upperBound(vector<int> &arr, int x, int n)
+{
+    // Write your code here.
+    // Upper Bound - Smallest index greater than arr[ind]>x
+    // Ex- [2,3,6,7,8,8,9,9,10] x=6
+    // Ans - 7
 
-	int low=0;
-	int high=n-1;
+    // Ex- [2,3,6,7,8,8,9,10,11] x=12
+    // Ans- Hypothetical ind -> 10
+    int low = 0;
+    int high = n - 1;
 
-	int curr_greater=n;
+    int curr_greater = n;
 
-	if(arr[low]>=x){
-		return low;
-	}
+    if (arr[low] >= x)
+    {
+        return low;
+    }
 
-	while(low<=high){
-		int mid=(low+high)/2;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
 
-		if(arr[mid]>x){
-			curr_greater=mid;
-			high=mid-1;
-		}
-		else if(arr[mid]<=x){
-			low=mid+1;
-		}
-	
-	}
-	return curr_greater;
+        if (arr[mid] > x)
+        {
+            curr_greater = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid] <= x)
+        {
+            low = mid + 1;
+        }
+    }
+    return curr_greater;
 }
